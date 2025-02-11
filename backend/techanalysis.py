30c0232d74948e68d22812a64b993a1466eae70b
Arvindh <arvindh92_gk@yahoo.com>
2025-02-11 21:26:12 +0100

    WIP

--- backend/techanalysis.py
+++ backend/techanalysis.py
@@ -1,1 +1,8 @@
 import talib
+
+op = df['CH_OPENING_PRICE'].astype(float)
+hi = df['CH_TRADE_HIGH_PRICE'].astype(float)
+lo = df['CH_TRADE_LOW_PRICE'].astype(float)
+cl = df['CH_CLOSING_PRICE'].astype(float)
+
+df['DOJI'] = talib.CDLDOJI(op, hi, lo, cl)
